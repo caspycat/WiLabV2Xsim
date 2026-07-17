abb92d16bfe965dc1e45db73b512f9c9b9570546
Caspian Toh <chester8991@gmail.com>
2026-07-17 16:28:04 +0800

    convert into MATLAB project structure

--- MatFilesUtilityLTE/updateBRidFile.m
+++
@@ -1,9 +0,0 @@
-function [BRid] = updateBRidFile(BRid,IDvehicle,indexNewVehicles)
-% Update BRid vector if using File Trace (variable number of vehicles)
-
-BRidIn = BRid(IDvehicle,:);
-BRidIn(indexNewVehicles,:)=-1;
-BRid = -2*ones(length(BRid(:,1)),length(BRid(1,:)));
-BRid(IDvehicle,:) = BRidIn;
-
-end
